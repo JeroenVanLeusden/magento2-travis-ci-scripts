61a99a9dc756ae40318d686c3cf9d93d94440779
Jeroen <jeroen@reachdigital.nl>
2018-11-08 15:42:58 +0100

    Update phpcs.sh

--- scripts/steps/script/phpcs.sh
+++ scripts/steps/script/phpcs.sh
@@ -4,10 +4,10 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 if [ -d $(pwd)/vendor/magento/marketplace-eqp ]; then
-  ./vendor/squizlabs/php_codesniffer/scripts/phpcs --config-set installed_paths $(pwd)/vendor/magento/marketplace-eqp
-  ./vendor/squizlabs/php_codesniffer/scripts/phpcs $MODULE_DEPLOY_PATH --standard=MEQP2
+  ./vendor/squizlabs/php_codesniffer/bin/phpcs --config-set installed_paths $(pwd)/vendor/magento/marketplace-eqp
+  ./vendor/squizlabs/php_codesniffer/bin/phpcs $MODULE_DEPLOY_PATH --standard=MEQP2
 elif [ -f $(pwd)/dev/tests/static/testsuite/Magento/Test/Php/_files/phpcs/ruleset.xml ]; then
-  ./vendor/squizlabs/php_codesniffer/scripts/phpcs $MODULE_DEPLOY_PATH --standard=$(pwd)/dev/tests/static/testsuite/Magento/Test/Php/_files/phpcs
+  ./vendor/squizlabs/php_codesniffer/bin/phpcs $MODULE_DEPLOY_PATH --standard=$(pwd)/dev/tests/static/testsuite/Magento/Test/Php/_files/phpcs
 elif [ -f $(pwd)/dev/tests/static/framework/Magento/ruleset.xml ]; then
-  ./vendor/squizlabs/php_codesniffer/scripts/phpcs $MODULE_DEPLOY_PATH --standard=$(pwd)/dev/tests/static/framework/Magento
+  ./vendor/squizlabs/php_codesniffer/bin/phpcs $MODULE_DEPLOY_PATH --standard=$(pwd)/dev/tests/static/framework/Magento
 fi
